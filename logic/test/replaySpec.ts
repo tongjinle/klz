e82637e4a910bfa4e6deb8c71bbd6b56c9669f8a
tongjinle@ruulai.com <tongjinle@ruulai.com>
2016-08-20 09:11:56 +0800

    ~ 开始写replay的测试

---
+++ logic/test/replaySpec.ts
@@ -0,0 +1,17 @@
+/// <reference path="../../typings/index.d.ts" />
+import {IPosition, IBox, IChessBoard, IChess, ISkill, IEffect, IMoveRecord, IEffectRecord, IRecord, IRecordFilter, IRecordMgr, IRangeGen, IAsk, IAnswer, IPlayer, IGame, ChessColor, ChessType, ChessStatus, PlayerStatus, SkillType, RecordType, AskType } from '../types';
+import _ = require('underscore');
+import chessList from '../chess/chessList';
+import skillList from '../skill/skillList';
+import ChessBoard from '../chessBoard/chessBoard';
+import Chess from '../chess/chess';
+import ReplayMgr from '../replayMgr';
+
+
+describe('replay',()=>{
+	let repMgr :ReplayMgr;
+
+	beforeEach(()=>{});
+
+	
+});
